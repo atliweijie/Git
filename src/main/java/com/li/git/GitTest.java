d7c47b8d5f60edc71bd5d48662dd32800ae3f4dc
lwj <lwj@li.com>
2022-06-23 14:32:47 +0800

    second commit

--- src/main/java/com/li/git/GitTest.java
+++ src/main/java/com/li/git/GitTest.java
@@ -3,5 +3,6 @@ package com.li.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello,git!");
+        System.out.println("hello,git2!");
     }
 }
